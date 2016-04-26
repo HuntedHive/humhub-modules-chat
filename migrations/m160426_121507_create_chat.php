39f32ffff3a22ab792816c391d33131c56d01374
Den Volkov <volkovdenya@gmail.com>
2016-04-26 15:18:13 +0300

    TC-9 - edit migration name

---
+++ migrations/m160426_121507_create_chat.php
@@ -0,0 +1,49 @@
+<?php
+
+class m160426_121507_create_chat extends EDbMigration
+{
+	public function up()
+	{
+		$this->createTable('wbs_chat', [
+			'id' => 'pk',
+			'user_id' => 'varchar(100) NOT NULL',
+			'text' => 'text NOT NULL',
+			'file' => 'text NOT NULL',
+			'created_at' => 'datetime NOT NULL',
+			'created_by' => 'int(11) NOT NULL',
+			'updated_at' => 'datetime NOT NULL',
+			'updated_by' => 'int(11) NOT NULL',
+		], ''
+		);
+
+		$this->createTable('wbs_smiles', [
+			'id' => 'pk',
+			'symbol' => 'varchar(50)',
+			'link' => 'text NOT NULL',
+			'created_at' => 'datetime NOT NULL',
+			'created_by' => 'int(11) NOT NULL',
+			'updated_at' => 'datetime NOT NULL',
+			'updated_by' => 'int(11) NOT NULL',
+		], ''
+		);
+
+		$this->addColumn('user', 'is_chating', "INT NOT NULL DEFAULT  '1'");
+	}
+
+	public function down()
+	{
+		echo "m160426_121507_create_chat does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
