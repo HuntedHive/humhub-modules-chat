1175983600dcd5668fbc573087694682c4b2e959
VolkovDenis <volkovdenya@gmail.com>
2016-09-05 15:48:27 +0300

    TC-134 fixed chat activity view

--- activities/views/ChatMessage.php
+++ activities/views/ChatMessage.php
@@ -1,9 +1,13 @@
 <?php
 
 use yii\helpers\Html;
-
-echo Yii::t('TasksModule.views_activities_TaskFinished', '<i class="fa fa-commenting-o color-chat" style="margin-right: 5px;color: #1895a4;vertical-align: middle;"></i> {userName} posted in <strong>live chat</strong> {answer}.', array(
-    '{userName}' => '<strong>' . $originator->displayName  . '</strong>',
-    '{answer}' => $source->getContentDescription()
-));
+use humhub\modules\chat\models\WBSChatSmile;
 ?>
+<div class="chat-activity">
+    <?php
+        echo Yii::t('app', '<i class="fa fa-commenting-o color-chat" style="margin-right: 5px;color: #1895a4;vertical-align: middle;"></i> {userName} posted in <strong>live chat</strong> {answer}.', array(
+            '{userName}' => '<strong>' . $originator->displayName  . '</strong>',
+            '{answer}' => WBSChatSmile::toSmile($source->getContentDescription()),
+        ));
+    ?>
+</div>
