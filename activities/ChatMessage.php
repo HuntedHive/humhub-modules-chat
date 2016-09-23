f9717d3f45d18d1ce787fbdeea0918550b6f607c
VolkovDenis <volkovdenya@gmail.com>
2016-09-23 13:24:43 +0300

    license

--- activities/ChatMessage.php
+++ activities/ChatMessage.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @license https://www.humhub.org/licences GNU AGPL v3
+ */
+
 namespace humhub\modules\chat\activities;
 
 use humhub\modules\activity\components\BaseActivity;
