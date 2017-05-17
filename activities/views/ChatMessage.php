538efc2b5130036ed38a5a324e667b963247f586
Katarzyna Fedevych <katarzynan@gmail.com>
2017-05-17 10:09:54 +0200

    TC-205 - minor HTML adjustments

--- activities/views/ChatMessage.php
+++ activities/views/ChatMessage.php
@@ -2,12 +2,9 @@
 
 use yii\helpers\Html;
 use humhub\modules\chat\models\WBSChatSmile;
+
+echo Yii::t('app', '<i class="fa fa-commenting-o color-chat" style="margin-right: 5px;color: #1895a4;vertical-align: middle;"></i> {userName} posted in <strong>live chat</strong> {answer}.', array(
+    '{userName}' => '<strong>' . $originator->displayName . '</strong>',
+    '{answer}' => WBSChatSmile::toSmile($source->getContentDescription()),
+));
 ?>
-<div class="chat-activity">
-    <?php
-        echo Yii::t('app', '<i class="fa fa-commenting-o color-chat" style="margin-right: 5px;color: #1895a4;vertical-align: middle;"></i> {userName} posted in <strong>live chat</strong> {answer}.', array(
-            '{userName}' => '<strong>' . $originator->displayName  . '</strong>',
-            '{answer}' => WBSChatSmile::toSmile($source->getContentDescription()),
-        ));
-    ?>
-</div>
