c51cb24511b564120241a75429d159f31a09cd92
Katarzyna Fedevych <katarzynan@gmail.com>
2017-05-18 12:54:09 +0200

    TC-205 - changing HTML

--- views/activities/ChatMessage.php
+++ views/activities/ChatMessage.php
@@ -1,5 +1,4 @@
 <?php if(!empty($user) && !empty($target) && (\Yii::app()->user->id != $target->created_by)) {  ?>
-    <div class="chat-activity">
         <?php $this->beginContent('application.modules_core.activity.views.activityLayout', array('activity' => $activity)); ?>
             <?php
                 echo Yii::t('TasksModule.views_activities_TaskCreated', '<i class="fa fa-commenting-o color-chat" style="margin-right: 5px;color: #1895a4;vertical-align: middle;"></i> {userName} posted in <strong>live chat</strong> "{message}".', array(
@@ -8,6 +7,5 @@
                 ));
             ?>
         <?php $this->endContent(); ?>
-    </div>
 <?php } ?>
 
